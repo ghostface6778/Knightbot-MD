5522c545f2e79e918bd3961f3e3be23aba2fcf3b
ghostface6778 <sunwukong141141@gmail.com>
2025-03-27 14:37:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Brian Moser', // Your name
-  ownerNumber: '94760722315', //Your number
+  ownerNumber: '94763299135', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
